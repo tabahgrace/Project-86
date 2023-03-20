3d7c53784c6127dc103820501036dc842f202031
tabahgrace <125412437+tabahgrace@users.noreply.github.com>
2023-03-20 17:00:22 -0400

    Add files via upload

---
+++ Tmain.js
@@ -0,0 +1,44 @@
+
+name_of_the_student_array = [];
+    
+function submit()
+{
+    var display_student_array = [];
+
+    for (var j = 1; j <= 4; j++) 
+    {
+        var name_of_the_student = document.getElementById("name_of_the_student_"+j).value;
+        console.log(name_of_the_student);
+        name_of_the_student_array.push(name_of_the_student);
+    }
+
+    console.log(name_of_the_student_array);
+
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) 
+    {
+        display_student_array.push("<h4>NAME - "+ name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array);
+    }
+
+    console.log(display_student_array);
+    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
+
+    var remove_commas = display_student_array.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+
+
+    document.getElementById("submit_button").style.display = "none";
+
+    var val = get-item_list(grades);
+    document.getElementById("scores").value=val;
+    document.getElementById("student-name").value="";
+    document.getElementById("score").value="";
+    document.getElementById("student_name").focus();
+
+    grades.sort();
+
+}
